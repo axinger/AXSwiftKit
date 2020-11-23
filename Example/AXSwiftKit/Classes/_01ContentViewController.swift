772d4a907fc5f61ec71a61120247bf2967d5351f
axinger <axingrun@outlook.com>
2020-11-23 14:58:05 +0800

    update…

--- Example/AXSwiftKit/Classes/_01ContentViewController.swift
+++ Example/AXSwiftKit/Classes/_01ContentViewController.swift
@@ -6,12 +6,11 @@
 //  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
-import UIKit
-//import ReactiveCocoa
+// import ReactiveCocoa
 import SnapKit
+import UIKit
 
 class _01ContentViewController: UIViewController {
-    
     lazy var containerView = UIView()
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -22,7 +21,6 @@ class _01ContentViewController: UIViewController {
     lazy var lastBottom: SnapKit.ConstraintItem = {
         if #available(iOS 11.0, *) {
             return self.view.safeAreaLayoutGuide.snp.top
-            
         } else {
             return self.view.snp.top
         }
@@ -37,25 +35,23 @@ class _01ContentViewController: UIViewController {
         view.addSubview(scrollView)
         scrollView.addSubview(containerView)
         
-        scrollView.snp.makeConstraints { (make) in
+        scrollView.snp.makeConstraints { make in
             make.edges.equalTo(UIEdgeInsets.zero)
         }
         
-        containerView.snp.makeConstraints { (make) in
+        containerView.snp.makeConstraints { make in
             make.width.height.equalTo(scrollView)
         }
         
         lastBottom = _test01(lastBottom)
         lastBottom = _test02(lastBottom)
         
-        containerView.snp.makeConstraints { (make) in
+        containerView.snp.makeConstraints { make in
             make.bottom.equalTo(lastBottom)
         }
     }
     
-    
     func _p00ButtonTopView(_ lastBottom: SnapKit.ConstraintItem, title: String?, handler: @escaping () -> Void) -> SnapKit.ConstraintItem? {
-        
         var title = title
         if (title?.count ?? 0) == 0 {
             title = "title"
@@ -65,14 +61,13 @@ class _01ContentViewController: UIViewController {
         btn.backgroundColor = UIColor.blue
         btn.setTitle(title, for: .normal)
         
-        btn.snp.makeConstraints { (make) in
+        btn.snp.makeConstraints { make in
             make.top.equalTo(lastBottom).offset(20)
             make.left.equalToSuperview().offset(10)
             make.right.equalToSuperview().offset(-10)
-            
         }
         /// 按钮事件
-        btn.reactive.controlEvents(UIControl.Event.touchUpInside).observeResult { (obj : Result<UIButton, Never>) in
+        btn.reactive.controlEvents(UIControl.Event.touchUpInside).observeResult { (obj: Result<UIButton, Never>) in
             let btn = try? obj.get()
             print(btn ?? "按钮事件错误")
         }
@@ -81,20 +76,17 @@ class _01ContentViewController: UIViewController {
     
     func _test01(_ lastBottom: SnapKit.ConstraintItem) -> SnapKit.ConstraintItem {
         return _p00ButtonTopView(lastBottom, title: "按钮1") {
-            
-        } ?? self.view.snp.top
+        } ?? view.snp.top
     }
     
     func _test02(_ lastBottom: SnapKit.ConstraintItem) -> SnapKit.ConstraintItem {
         var lastBottom = lastBottom
         
-        lastBottom =  _p00ButtonTopView(lastBottom, title: "按钮2") {
-            
-        } ?? self.view.snp.top
+        lastBottom = _p00ButtonTopView(lastBottom, title: "按钮2") {
+        } ?? view.snp.top
         
-        lastBottom =  _p00ButtonTopView(lastBottom, title: "按钮3") {
-            
-        } ?? self.view.snp.top
+        lastBottom = _p00ButtonTopView(lastBottom, title: "按钮3") {
+        } ?? view.snp.top
         
         return lastBottom
     }
